7913cd6e45452562b60b6452f587342049c615a2
Monica <alip.monica@gmail.com>
2023-03-16 11:02:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<unistd.h>
 /**
  * main-Entry point
- * Return Always 1(success)
+ * Return Always 0(success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
 return (1);
 }
