fd13a90e8ad6a83cdaa8479c50c3501cc31013ed
Monica <alip.monica@gmail.com>
2023-03-16 09:15:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
+
 /**
- * my_function - This is a description
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-#include<stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
