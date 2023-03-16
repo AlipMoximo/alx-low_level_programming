c36c2d75793cf9257918e82da65cc8b3297daa66
Monica <alip.monica@gmail.com>
2023-03-16 08:57:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
+/**
+ * my_function - This is a description
+ */
 #include<stdio.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
