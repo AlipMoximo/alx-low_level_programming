c238d72af03062dade49779931e19ed05b366905
Monica <alip.monica@gmail.com>
2023-03-17 22:25:42 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Prints the alphabet
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char m = 'a';
+while (m <= 'z')
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
