31cb7f642153ad6c7204cfc0f3249d556271db43
Monica <alip.monica@gmail.com>
2023-03-18 16:36:17 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,29 +5,16 @@
  */
 int main(void)
 {
-int i, j, k, l;
-for (i = 0; i < 10; i++)
+int i;
+for (i = 48; i < 58; i++)
 {
-for (j = i + 1; j < 10; j++)
+putchar(i);
+if (i != 57)
 {
-for (k = j + 1; k < 10; k++)
-{
-for (l = k + 1; l < 10; l++)
-{
-putchar(i + '0');
 putchar(',');
 putchar(' ');
-putchar(j + '0');
-putchar(',');
-putchar(' ');
-putchar(k + '0');
-putchar(',');
-putchar(' ');
-putchar(l + '0');
+}
+}
 putchar('\n');
-}
-}
-}
-}
 return (0);
 }
