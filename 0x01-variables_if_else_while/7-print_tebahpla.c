3cfe8f6c25d4f8a5e6ef9f4510593434e48879c1
Monica <alip.monica@gmail.com>
2023-03-18 12:00:35 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Printing lowercase numbers in reverser z-a
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+char m;
+for (m = 'z'; m >= 'a'; m--)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
+
