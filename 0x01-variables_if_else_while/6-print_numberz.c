03311b958358857c98fd4e1235949c509219fbb1
Monica <alip.monica@gmail.com>
2023-03-18 11:01:12 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main-Print numbers 0-9 using char
+ * Return:0(Sucess)
+ */
+int main(void)
+{
+int m;
+for (m = 0; m < 10; m++)
+{
+putchar (m + '0');
+}
+putchar ('\n');
+return (0);
+}
