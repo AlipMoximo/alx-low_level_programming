75a3769da0c5d237449c3f46e583f2445bd55457
Monica <alip.monica@gmail.com>
2023-03-18 16:26:50 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main-All posible single digits seoerated by ,
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+int i, j, k, l;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+for (l = k + 1; l < 10; l++)
+{
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+putchar(k + '0');
+putchar(',');
+putchar(' ');
+putchar(l + '0');
+putchar('\n');
+}
+}
+}
+}
+return (0);
+}
