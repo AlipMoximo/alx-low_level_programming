c59b8151691b7bc55ed9b56dc44d652903fe712d
Monica <alip.monica@gmail.com>
2023-03-21 16:56:26 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit and stores
+ * @n: the chosen number
+ * Return: Value of last digit
+ */
+int print_last_digit(int n)
+{
+int x = n % 10;
+if (x < 0)
+{
+x = x * -1;
+}
+_putchar(x + '0');
+return (x);
+}
