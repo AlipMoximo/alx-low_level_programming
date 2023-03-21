e8906fadcb563e3caf2c7ff61e83fd295c94c3b9
Monica <alip.monica@gmail.com>
2023-03-21 11:49:17 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ * print_alphabet-lowercase
+ * Return:void
+ */
+#include "main.h"
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
