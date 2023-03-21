1bd7a00ed2f55e9b4e6b9844250564ac4612361b
Monica <alip.monica@gmail.com>
2023-03-21 12:36:49 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int_isslower - checks for lowercase
+ * C :character being checked
+ * Returns 1 if c is lowercase and 0 if uppercase
+ */
+int_isslower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+Return(1);
+}
+Return(0);
+}
