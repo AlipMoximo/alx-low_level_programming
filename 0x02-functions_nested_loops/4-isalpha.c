171d55a6dd8a3e302cc015abc1b100f69a366f78
Monica <alip.monica@gmail.com>
2023-03-21 13:12:42 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: The character to be checked
  * Return: 1 for alhpabetic character or 0 for anything else
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 {
