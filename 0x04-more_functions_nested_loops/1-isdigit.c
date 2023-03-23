a4752fb2d638de93a4051952358db2286638fe3e
Monica <alip.monica@gmail.com>
2023-03-23 09:16:10 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit-checks for a digit 0 to 9
+ * @c:is the digit
+ * Return: 1 if c is a digit or 0 is not
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 56)
+{
+return (1);
+}
+return (0);
+}
