b18774081ceb87b986fe67beb8521c915cbbeb58
Monica <alip.monica@gmail.com>
2023-03-23 10:12:09 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers-prints numbers 0 to 9 without 2 and 4
+ * @x:value
+ */
+void print_most_numbers(void)
+{
+int x = 0;
+for (; x <= 9, x++)
+{
+if (x != 2 && x != 4)
+{
+continue;
+}
+else
+{
+_putchar(x + '0');
+}
+}
+_putchar('\n');
+}
