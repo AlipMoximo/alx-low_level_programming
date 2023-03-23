6ed37df57d7ed8d406bdbbac0b76795cbf00580d
Monica <alip.monica@gmail.com>
2023-03-23 09:03:29 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper- checks for uppercase character
+ * @c: the character
+ * Return: 1 if uppercase and 0 if lowercase
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+
+return (0);
+}
