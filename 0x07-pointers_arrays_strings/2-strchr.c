75c1eafa789b9bdf10aebef4628971bf66cc2c06
Monica <alip.monica@gmail.com>
2023-04-04 17:42:25 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-check the code
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+char *s = "welcome"
+char *m;
+m = _strchr(s, 'l');
+if (m != NULL)
+{
+printf("%s\n", m);
+}
+return (0);
+}
