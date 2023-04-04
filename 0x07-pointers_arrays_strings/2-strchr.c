a7a99a4c5478b03d9a6a1376392ae65994fe41e5
Monica <alip.monica@gmail.com>
2023-04-04 18:14:22 +0000

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,16 +2,18 @@
 /**
  * _strchr - Entry point
  * @s: input
- * @c:input
+ * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
 int i = 0;
-for (; s[i] >= '\0'; i++
+
+for (; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
 return (&s[i]);
 }
 return (0);
 }
+
